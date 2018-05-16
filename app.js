392b0020635c6df84c576918f345e60f0fbfe924
Catherine Lin <catherine.lin.93@gmail.com>
2018-05-16 11:56:35 -0700

    increments work

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
+    let breakCounter = parseInt($('#breakNum').html());
+    let sessionCounter = parseInt($('#sessionNum').html());
 
+    // break length
+    $('#breakPlus').click(() => {
+        breakCounter++;
+        $('#breakNum').html(breakCounter);
+    })
+    $('#breakMinus').click(() => {
+        if (breakCounter > 0) {
+            breakCounter--;
+            $('#breakNum').html(breakCounter);
+        }
+    })
 
-    
+    // session length
+    $('#sessionPlus').click(() => {
+        sessionCounter++;
+        $('#sessionNum').html(sessionCounter);
+    })
+    $('#sessionMinus').click(() => {
+        if(sessionCounter > 0) {
+            sessionCounter--;
+            $('#sessionNum').html(sessionCounter);
+        }
+    })
+
 });
